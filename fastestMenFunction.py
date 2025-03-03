a7670d8cf9541668d3399bb0f0212ad8822c09ce
sky-ash <5kyl3r@proton.me>
2025-03-03 15:36:27 +0100

    Update fastestMenFunction.py

--- fastestMenFunction.py
+++ fastestMenFunction.py
@@ -18,34 +18,37 @@ except psycopg2.Error as error:
     print("Fehler beim Verbinden mit der Datenbank: \n" + str(error))
     sys.exit()
 
-def get_fastest_male_runner(event):
+with myConnection.cursor() as myCursor:
+    try:
+        # Prepared Statement für die Abfrage
+        myCursor.execute("PREPARE fastest_runner AS "
+                        "SELECT name FROM public.results "
+                        "WHERE gender = 'M' AND event = $1 "
+                        "ORDER BY result ASC LIMIT 1;")
+    except psycopg2.Error as error:
+        print("Fehler bei der Query Preparation: \n" + str(error))
+
+def get_fastest_male_runner(events):
     try:
         with myConnection.cursor() as myCursor:
-            try:
-                # Prepared Statement für die Abfrage
-                myCursor.execute("PREPARE fastest_runner AS "
-                                "SELECT name FROM public.results "
-                                "WHERE gender = 'M' AND event = $1 "
-                                "ORDER BY result ASC LIMIT 1;")
-                
-                # Ausführung des Prepared Statements
-                myCursor.execute("EXECUTE fastest_runner (%s);", (event,))
-                result = myCursor.fetchone()
-                if result:
-                    print(f"Schnellster Mann {event}: {result[0]}")
-                else:
-                    print(f"Keine Ergebnisse für {event}")
-                
-            except psycopg2.Error as error:
-                print("Fehler bei der Abfrage mit SELECT: \n" + str(error))
+            for event in events:
+                try:
+                    # Ausführung des Prepared Statements
+                    myCursor.execute("EXECUTE fastest_runner (%s);", (event,))
+                    result = myCursor.fetchone()
+                    if result:
+                        print(f"Schnellster Mann {event}: {result[0]}")
+                    else:
+                        print(f"Keine Ergebnisse für {event}")
+
+                except psycopg2.Error as error:
+                    print("Fehler bei der Abfrage mit SELECT: \n" + str(error))
             
     except psycopg2.Error as error:
         print("Fehler beim Zugriff auf die Datenbank: \n" + str(error))
 
 # Funktion dreimal ausführen
-get_fastest_male_runner('100M Men')
-get_fastest_male_runner('200M Men')
-get_fastest_male_runner('400M Men')
-
-# Verbindung schließen
-myConnection.close()
+get_fastest_male_runner(['100M Men', '200M Men', '400M Men'])
+#get_fastest_male_runner('100M Men')
+#get_fastest_male_runner('200M Men')
+#get_fastest_male_runner('400M Men')
